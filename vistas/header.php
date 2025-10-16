59dc89e44503ad877a7da27e48c5a225dfb29120
johaoRamirezRoyal <hernando.ramirez@royalschool.edu.co>
2025-10-16 09:32:09 -0500

    Agregado icono temporal de la copa navidad

--- vistas/header.php
+++ vistas/header.php
@@ -11,7 +11,7 @@
     <meta name="googlebot" content="noindex">
     <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
     <meta name="msapplication-TileColor" content="#ffffff">
-    <link rel="icon" type="image/x-icon" href="<?=PUBLIC_PATH?>img/engranajes.gif"> <!-- Buscar otro gif para la carga -->
+    <link rel="icon" type="image/x-icon" href="<?=PUBLIC_PATH?>img/copa_navidad_icono.ico"> <!-- Buscar otro gif para la carga -->
     <meta name="theme-color" content="#fff999">
     <!-- Custom fonts for this template-->
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet"> <!-- Buscar otra fuente en google fonts -->
