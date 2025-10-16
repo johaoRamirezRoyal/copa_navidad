e83083efce80405f5db8472f9c00a77d484db636
johaoRamirezRoyal <hernando.ramirez@royalschool.edu.co>
2025-10-16 09:29:31 -0500

    Añadido Jquery y configuraciones de Bootstrap

---
+++ vistas/script_and_final.php
@@ -0,0 +1,8 @@
+<!-- Bootstrap core JavaScript-->
+<script src="<?=PUBLIC_PATH?>vendor/jquery/jquery.min.js"></script>
+<script src="<?=PUBLIC_PATH?>/js/jquery.base64.js"></script>
+<script src="<?=PUBLIC_PATH?>vendor/bootstrap-5.3.8/js/bootstrap.bundle.min.js"></script>
+
+<!-- Core plugin JavaScript-->
+<script src="<?=PUBLIC_PATH?>vendor/jquery-easing/jquery.easing.min.js"></script>
+
