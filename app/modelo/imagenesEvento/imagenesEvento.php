d7df16cd66542ee6a54a7f935689c1618d9f8cca
johaoRamirezRoyal <hernando.ramirez@royalschool.edu.co>
2025-11-25 08:46:08 -0500

    Actualización y fix de la query para obtener las imagenes

--- app/modelo/imagenesEvento/imagenesEvento.php
+++ app/modelo/imagenesEvento/imagenesEvento.php
@@ -5,7 +5,7 @@ class imagenesEvento extends conexion {
     public static function obtenerImagenesDeporteModel($deporte){
         $tabla = "imagen_evento";
         $cnx = conexion::singleton_conexion();
-        $cmdsql = "SELECT * FROM $tabla WHERE id_deporte = :deporte ORDER BY id_imagen_evento DESC";
+        $cmdsql = "SELECT * FROM $tabla WHERE id_deporte = :deporte ORDER BY id_imagen_evento DESC LIMIT 10";
         try{
             $preparado = $cnx->preparar($cmdsql);
             $preparado->bindParam(":deporte", $deporte, PDO::PARAM_INT);
